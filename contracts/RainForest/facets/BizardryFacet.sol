bcdd7716834e25aa66b4d33b2ee1d85e8cdfebad
jutalik <50254404+jutalik@users.noreply.github.com>
2023-11-13 23:37:42 +0900

    bizardry facet writing ...

--- contracts/RainForest/facets/BizardryFacet.sol
+++ contracts/RainForest/facets/BizardryFacet.sol
@@ -3,5 +3,18 @@ pragma solidity ^0.8.22;
 
 import {AppStorage, Modifiers, LibAppStorage} from "../libraries/LibAppStorage.sol";
 import "../interfaces/IBizardry.sol";
+import "@openzeppelin/contracts/proxy/beacon/BeaconProxy.sol";
 
-contract BizardryFacet {}
+contract BizardryFacet is Modifiers{
+
+    function createERC20(string memory _name, string memory _symbol, uint256 _initialValue) external onlyOwner {
+        AppStorage storage s = LibAppStorage.diamondStorage();
+        
+        BeaconProxy proxy = new BeaconProxy(
+            s.ERC20_Impl[0].proxy,
+            abi.encodeWithSignature("initialize(string,string,uint256)", _name, _symbol,_initialValue)
+        );
+
+
+    }
+}
