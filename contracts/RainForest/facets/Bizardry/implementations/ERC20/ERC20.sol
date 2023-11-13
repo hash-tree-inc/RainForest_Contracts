3317404d83db4cc8c9da712be29df735b3ab4ed6
jutalik <50254404+jutalik@users.noreply.github.com>
2023-11-14 02:42:03 +0900

    .

--- contracts/RainForest/facets/Bizardry/implementations/ERC20/ERC20.sol
+++ contracts/RainForest/facets/Bizardry/implementations/ERC20/ERC20.sol
@@ -3,13 +3,17 @@ pragma solidity ^0.8.22;
 
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
-
+import "../interfaces/IVersion.sol";
 contract ERC20 is Initializable, ERC20Upgradeable {
+    uint constant bizardryVersion = 1;
+
     function initialize(
         string memory _name,
-        string memory _symbol
+        string memory _symbol,
+        uint256 _initialValue
     ) public initializer {
         __ERC20_init(_name, _symbol);
+        _mint(msg.sender, _initialValue);
     }
 
     function mint(address account, uint256 amount) external {
@@ -18,5 +22,9 @@ contract ERC20 is Initializable, ERC20Upgradeable {
 
     function burn(address account, uint256 amount) external {
         _burn(account, amount);
+    }
+
+    function getBizardryVersion() external pure returns (uint) {
+        return bizardryVersion;
     }
 }
--- contracts/shared/interfaces/IVersion.sol
+++ contracts/shared/interfaces/IVersion.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.18;
 
 interface IVersion {
-    function getVersion() external view returns (uint256);
+    function getBizardryVersion() external view returns (uint256);
 
-    function setVersion(uint256 _version) external returns (bool);
+    function setgetBizardryVersion(uint256 _version) external returns (bool);
 }
